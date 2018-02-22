be715c4a582e20db9902a8998b5b77b25b2feefa
omarfs <omarfs@abc.com>
2018-02-22 20:47:26 +0300

    Initial commit

---
+++ src/playground/redux-101.js
@@ -0,0 +1,91 @@
+import { createStore } from 'redux';
+
+const incrementCount = ({ incrementBy = 1} = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+const setCount = ({ count } ) => ({
+  type: 'SET',
+  count
+});
+
+const countReducer = (state = {count:0 }, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return {
+      count : state.count + action.incrementBy
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - action.decrementBy
+      }; 
+    case 'SET':
+      return {
+        count: action.count
+      }  
+    case 'RESET':
+      return {
+        count: 0
+      };  
+    default:
+      return {
+        count: state.count
+      }; 
+  }
+};
+
+const store = createStore(countReducer);
+
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
+
+store.dispatch(incrementCount({incrementBy:7}));
+
+// store.dispatch({
+//   type: 'INCREMENT'
+// });
+store.dispatch(incrementCount());
+
+// store.dispatch({
+//   type: 'RESET'
+// });
+
+store.dispatch(resetCount());
+
+//unsubscribe();
+
+// store.dispatch({
+//   type: 'DECREMENT'
+// });
+
+store.dispatch(decrementCount());
+
+// store.dispatch({
+//   type: 'DECREMENT',
+//   decrementBy: 10
+// });
+store.dispatch(decrementCount({decrementBy: 7}));
+
+// store.dispatch({
+//   type: 'SET',
+//   count: 101
+// });
+
+store.dispatch(setCount({ count: 101}));
